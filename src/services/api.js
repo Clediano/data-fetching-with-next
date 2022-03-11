479a65b8ed12bbdc08fdcbcb164aed6d9227a7b4
Estefenon <clediano.estefenon@capgemini.com>
2022-03-11 10:06:20 -0300

    changed environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SECRET_KEY = process.env.API_KEY
+const SECRET_KEY = process.env.NEXT_PUBLIC_API_KEY
 
 export const api = axios.create({
     baseURL: 'https://api.pexels.com/v1/',
