57e7e35ae9d3a1097a798975e776000e7457526f
Estefenon <clediano.estefenon@capgemini.com>
2022-03-11 09:29:53 -0300

    change secret key

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const SECRET_KEY = process.env.API_KEY
+
 export const api = axios.create({
     baseURL: 'https://api.pexels.com/v1/',
     headers: {
-        'Authorization': '563492ad6f91700001000001873235568afd44b0ab7a0e7f63ca302d'
+        'Authorization': SECRET_KEY
     }
 })
